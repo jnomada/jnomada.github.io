5b40461d5b1ebe346dd578f661e1c2db0fc49aec
James <jsnomada@outlook.com>
2019-03-14 09:33:55 +0100

    Robofriends dev version with Redux in progress

--- robofriends-dev/src/index.js
+++ robofriends-dev/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import App from './containers/App';
-//import { robots } from './robots';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
+
+
 ReactDOM.render(
-				<App />
+				<Provider store={store}>
+					<App />
+				</Provider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
